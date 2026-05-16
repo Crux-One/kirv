33dfca4c4e8cc1be264020612a7c6b89787c10eb
Crux One <Crux-One@users.noreply.github.com>
2026-05-16 16:56:11 +0900

    refactor: remove unreachable non-macOS main

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,8 @@
-#[cfg(target_os = "macos")]
 use kirv::service;
 
-#[cfg(target_os = "macos")]
 fn main() {
     if let Err(err) = service::run() {
         eprintln!("{err}");
         std::process::exit(1);
     }
 }
-
-#[cfg(not(target_os = "macos"))]
-fn main() {}
